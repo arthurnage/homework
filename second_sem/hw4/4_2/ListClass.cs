2fe5aa1ebf83764a85c06def85fcc973a2ddac99
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-03-17 01:05:16 +0300

    Added files via upload
    task 4_2

---
+++ second_sem/hw4/4_2/ListClass.cs
@@ -0,0 +1,106 @@
+using System;
+
+namespace MyWorks
+{
+	// class for keeping data in a list and working with it
+	public class List
+	{
+		private ListElement top;
+
+		// class appearing as a list element
+		private class ListElement
+		{
+			//auto properties
+			public int Value { get; set; }
+			public ListElement Next { get; set; }
+
+			//constructor
+			public ListElement(int value, ListElement nextElement)
+			{
+				Value = value;
+				Next = nextElement;
+			}
+		}
+
+		// constructor
+		public List() { }
+
+		// adds a new element to the list
+		public virtual void Push(int value)
+		{
+			ListElement newElement = new ListElement(value, top);
+			newElement.Next = top;
+			top = newElement;
+		}
+
+		// deletes a top element; returns its value
+		public int Pop()
+		{
+			if (top == null)
+			{
+				Console.WriteLine("list is empty");
+				return 0;
+			}
+			int value = top.Value;
+			top = top.Next;
+			return value;
+		}
+
+		// returns a value of the top of the list
+		public int Value()
+		{
+			return top.Value;
+		}
+
+		// checks is the list empty
+		public bool IsEmpty()
+		{
+			return top == null;
+		}
+
+		// print list
+		public void Print()
+		{
+			ListElement current = top;
+			while (current != null)
+			{
+				Console.Write("{0} ", current.Value);
+				current = current.Next;
+			}
+		}
+
+		// checks does element belong to the list
+		public bool CheckForBelong(int value)
+		{
+			ListElement current = top;
+			while (current != null)
+			{
+				if (current.Value == value)
+				{
+					break;
+				}
+				current = current.Next;
+			}
+			return current != null;
+		}
+
+		// removes element from the list
+		public virtual void Remove(int value)
+		{
+			ListElement current = top;
+			if (current.Value == value)
+			{
+				top = current.Next;
+			}
+			while (current.Next != null)
+			{
+				if (current.Next.Value == value)
+				{
+					current.Next = current.Next.Next;
+					return;
+				}
+				current = current.Next;
+			}
+		}
+	}
+}
---
+++ second_sem/hw4/4_2/UniqueListClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyWorks
+{
+	// a list class without the equal elements
+	public class UniqueList : List
+	{
+		// adding an exception to Push method
+		public override void Push(int value)
+		{
+			if (CheckForBelong(value))
+			{
+				throw new AddingElementException("the list already has this element");
+			}
+			else
+			{
+				base.Push(value);
+			}
+		}
+
+		// adding an exception to Remove method
+		public override void Remove(int value)
+		{
+			if (!CheckForBelong(value))
+			{
+				throw new DeletingElementException("the list does not have this element");
+			}
+			else
+			{
+				base.Remove(value);
+			}
+		}
+	}
+}
+
