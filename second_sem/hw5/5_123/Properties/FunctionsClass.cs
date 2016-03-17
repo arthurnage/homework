8b37f666bafc62cf7f2d2c45091ab99e9bacd6c4
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-03-18 01:13:56 +0300

    hw5
    tasks 5_1, 5_2, 5_3

---
+++ second_sem/hw5/5_123/Properties/FunctionsClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MyWorks
+{
+	// class for using generic-delegats functions
+	public class Function
+	{
+		// changes all the list elements
+		public static List Map(List list, Func<int, int> Function)
+		{
+			List newList = new List();
+			while (!list.IsEmpty())
+			{
+				newList.Push(Function(list.Pop()));
+			}
+			while (!newList.IsEmpty())
+			{
+				list.Push(newList.Pop());
+			}
+			return list;
+		}
+
+		// deletes elements froms list in dependance of bool function
+		public static List Filter(List list, Func<int, bool> Function)
+		{
+			List newList = new List();
+			while (!list.IsEmpty())
+			{
+				int value = list.Pop();
+				if (Function(value))
+				{
+					newList.Push(value);
+				}
+			}
+			while (!newList.IsEmpty())
+			{
+				list.Push(newList.Pop());
+			}
+			return list;
+		}
+
+		// returns a multiplicated value from the list elements
+		public static int Fold(List list, int value, Func<int, int, int> Function)
+		{
+			while (!list.IsEmpty())
+			{
+				int number = list.Pop();
+				value = Function(value, number);
+			}
+			return value;
+		}
+	}
+}
+
