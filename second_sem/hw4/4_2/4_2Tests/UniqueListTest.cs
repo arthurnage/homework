f557dc824996a3c2557742662582bb13c9b62043
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-04-07 08:41:45 +0300

    additions to 4_2
    tests are done

---
+++ second_sem/hw4/4_2/4_2Tests/UniqueListTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MyWorks
+{
+	[TestFixture()]
+	public class UniqueListTest
+	{
+		private UniqueList list;
+
+		[SetUp()]
+		public void Initialise()
+		{
+			list = new UniqueList();
+		}
+
+		[Test()]
+		[ExpectedException(typeof(AddingElementException))]
+		public void PushTest()
+		{
+			list.Push(1);
+			list.Push(1);
+		}
+
+		[Test()]
+		[ExpectedException(typeof(DeletingElementException))]
+		public void RemoveAndPopTest()
+		{
+			list.Remove(2);
+			list.Push(3);
+			list.Pop();
+		}
+	}
+}
+
