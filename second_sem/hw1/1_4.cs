2cbb3db823325a552a27306fa31e7a95e5ffcb0c
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-04-07 08:57:41 +0300

    adding to 1_4
    (suddenly) i found out that i havent add this task to commit 3 weaks ago (or more)

--- second_sem/hw1/1_4.cs
+++ second_sem/hw1/1_4.cs
@@ -1,69 +1,94 @@
 using System;
 
-namespace _4
+namespace MyWorks
 {
 	// class for matrix output in spiral form
 	class MainClass
 	{
-		// main method
 		public static void Main (string[] args)
 		{
 			const int matrixSize = 5;
-			const int centerX = 2;
-			const int centerY = 2;
 			int[,] matrix = new int[matrixSize, matrixSize]   
-			{{17, 18, 19, 20, 21}, 
-			{16, 5, 6, 7, 22},
-			{15, 4, 1, 8, 23}, 
-			{14, 3, 2, 9, 24}, 
-			{13, 12, 11, 10, 25}};
-			int x = centerX;
-			int y = centerY;
+				{{17, 18, 19, 20, 21}, 
+				{16, 5, 6, 7, 22},
+				{15, 4, 1, 8, 23}, 
+				{14, 3, 2, 9, 24}, 
+				{13, 12, 11, 10, 25}};
+			SpiralPrint(matrix, matrixSize);
+		}
+
+		// prints the matrix in a spiral form
+		public static void SpiralPrint(int[,] matrix, int matrixSize)
+		{
+			int x = matrixSize / 2;//middle x
+			int y = matrixSize / 2;//middle y
+			int centerX = matrixSize / 2;
+			int centerY = matrixSize / 2;
 			int orientationFlag = 1;
-			while (orientationFlag < matrixSize)
+			int number = 0;
+			while (orientationFlag <= matrixSize / 2)
 			{
-				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag)); ++i)
+				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag) &&
+					(x < matrixSize) && (y < matrixSize) && (x >= 0) && (y >= 0)); ++i)
 				{
 
-					if (Math.Abs(centerX - x) < orientationFlag) 
+					if (Math.Abs(centerX - x) < orientationFlag)
 					{
 						Console.Write(matrix[x, y]);
 						Console.Write(" ");
+						number++;
 					}
 					x++;
 				}
 				x--;
-				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag)); ++i)
+				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag) &&
+					(x < matrixSize) && (y < matrixSize) && (x >= 0) && (y >= 0)); ++i)
 				{
 					if (Math.Abs(centerY - y) < orientationFlag)
 					{
 						Console.Write(matrix[x, y]);
 						Console.Write(" ");
+						number++;
 					}
 					y--;
 				}
 				y++;
-				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag)); ++i)
+				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag) &&
+					(x < matrixSize) && (y < matrixSize) && (x >= 0) && (y >= 0)); ++i)
 				{
 					if (centerX - x < orientationFlag)
 					{
 						Console.Write(matrix[x, y]);
 						Console.Write(" ");
+						number++;
 					}
 					x--;
 				}
 				x++;
-				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag)); ++i)
+				for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag) &&
+					(x < matrixSize) && (y < matrixSize) && (x >= 0) && (y >= 0)); ++i)
 				{
 					if (y - centerY < orientationFlag)
 					{
 						Console.Write(matrix[x, y]);
 						Console.Write(" ");
+						number++;
 					}
 					y++;
 				}
-				y--;
+				y--; 
 				orientationFlag++;
+			}
+			for (int i = 0; ((Math.Abs(centerX - x) <= orientationFlag) && (Math.Abs(centerY - y) <= orientationFlag) &&
+				(x < matrixSize) && (y < matrixSize) && (x >= 0) && (y >= 0)); ++i)
+			{
+				if (Math.Abs(centerX - x) < orientationFlag)
+				{
+					Console.Write(matrix[x, y]);
+					Console.Write(" ");
+					number++;
+				}
+				x++;
 			}
 		}
 	}
