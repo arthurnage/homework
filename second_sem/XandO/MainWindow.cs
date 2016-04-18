dd3956c70df076adb21b7306a525c87939bce97b
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-04-18 18:29:30 +0300

    control work 1 (2nd try)
    task with X and O game

---
+++ second_sem/XandO/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	private bool sign; // 0 when it's X, 1 when it's O
+	private short count; //when count is 9 the game ends
+
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void OnButtonClicked(object sender, EventArgs e)
+	{
+		var button = (Button)sender;
+		if (button.Label == null)
+		{
+			button.Label = sign ? "O" : "X";
+			sign = !sign;
+			count++;
+			if (count == 9)
+			{
+				resultScreen.Text = "the end";
+			}
+		}
+	}
+}
