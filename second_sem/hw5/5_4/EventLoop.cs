6152705445586883f303f561509685b2ea86b7a5
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-04-21 13:08:13 +0400

    task 5_4
    task about the cursor in console

---
+++ second_sem/hw5/5_4/EventLoop.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MyWorks
+{
+	public class EventLoop
+	{
+		private int positionY = Console.CursorLeft; // vertical cursor position
+		private int positionX = Console.CursorTop; // horisontal cursor position
+
+		// eventLoop body
+		public void Run()
+		{
+			while (true)
+			{
+				var key = Console.ReadKey();
+				switch (key.Key)
+				{
+				case ConsoleKey.LeftArrow:
+					{
+						if (positionX > 0)
+						{
+							positionX -= 1;
+							Console.SetCursorPosition(positionX, positionY);
+						}
+					}
+					break;
+				case ConsoleKey.RightArrow:
+					{
+						if (positionX < Console.LargestWindowWidth - 1)
+						{
+							positionX += 1;
+							Console.SetCursorPosition(positionX, positionY);
+						}
+					}
+					break;
+				case ConsoleKey.DownArrow:
+					{
+						if (positionY < Console.LargestWindowHeight - 1)
+						{
+							positionY += 1;
+							Console.SetCursorPosition(positionX, positionY);
+						}
+					}
+					break;
+				case ConsoleKey.UpArrow:
+					{
+						if (positionY > 0)
+						{
+							positionY -= 1;
+							Console.SetCursorPosition(positionX, positionY);
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+}
+
