6416b3d40eed9426134bd7ca11f5b4bc763389c6
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-05-11 22:21:07 +0300

    adding to hw6
    task 6_2 were added

---
+++ second_sem/hw6/6_2/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+using System.Timers;
+
+public partial class MainWindow: Gtk.Window
+{
+	private Timer timer;
+
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		timer = new Timer();
+		timer.Start();
+		timer.Elapsed += SetTime;
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void SetTime(object sender, EventArgs e)
+	{
+		watch.Text = DateTime.Now.ToString();
+	}
+}
