c9ef26d4cb6832b3f6f446ad924ecddae6489229
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2016-05-11 22:17:38 +0300

    hw7
    tasks 7_1 and 7_2 were added

---
+++ second_sem/hw7/7_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyWorks
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var set = new Set<int>();
+			var newSet = new Set<int>();
+			set.Add(1);
+			set.Add(2);
+			set.Add(3);
+			newSet.Add(3);
+			newSet.Add(4);
+			newSet.Add(5);
+			newSet = set.Union(newSet);
+			for (int i = 1; i <= 5; ++i)
+			{
+				Console.WriteLine(newSet.IsBelong(i));
+			}
+		}
+	}
+}
