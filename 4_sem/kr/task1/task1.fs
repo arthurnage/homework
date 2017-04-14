f0339045a05a65e0a4cd4a925c01cdc001639b49
Arthur Nagaev <arthur.nagaev.m@gmail.com>
2017-04-14 13:06:44 +0300

    Add files via upload
    control work

---
+++ 4_sem/kr/task1/task1.fs
@@ -0,0 +1,13 @@
+﻿module prog =
+    let mySeq =
+        let rec numbers n =
+            seq {
+                yield n
+                yield! numbers n }
+        let rec loop i =
+            seq {
+                yield! Seq.take i (numbers i)
+                yield! loop (i + 1) }
+        loop 1
+    let z = Seq.take 50 mySeq
+    Seq.iter (fun a -> printf "%A " a) z
